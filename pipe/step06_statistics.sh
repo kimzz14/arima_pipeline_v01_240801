6c4938c2d0e6f053ea1edd5ae677faacc964a841
kimzz14 <kimzz1004@gmail.com>
2024-08-06 11:03:02 +0900

    step06 bug fix

--- pipe/step06_statistics.sh
+++ pipe/step06_statistics.sh
@@ -4,4 +4,4 @@ MIN_MAPQ=10
 STATS='./script/mapping_pipeline/get_stats.pl'
 
 samtools index ${readID}.5P.aboveQ${MIN_MAPQ}.sorted.RG.dedupped.bam
-perl $STAT ${readID}.5P.aboveQ${MIN_MAPQ}.sorted.RG.dedupped.bam > ${readID}.5P.aboveQ${MIN_MAPQ}.sorted.RG.dedupped.bam.stats
+perl $STATS ${readID}.5P.aboveQ${MIN_MAPQ}.sorted.RG.dedupped.bam > ${readID}.5P.aboveQ${MIN_MAPQ}.sorted.RG.dedupped.bam.stats
